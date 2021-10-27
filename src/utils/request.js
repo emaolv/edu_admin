c78ca9abe60380f87404bb12783e6fe35d1972ce
emaolv <233103498@qq.com>
2021-10-27 13:20:25 +0800

    指定本地服务器地址

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,7 +5,9 @@ import { getToken } from '@/utils/auth'
 
 // create an axios instance
 const service = axios.create({
-  baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
+  // 这里替换成自己接口的服务器地址 并且把.env.development中的VUE_APP_BASE_API参数置为空
+  // baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
+  baseURL: 'http://localhost:8110',
   // withCredentials: true, // send cookies when cross-domain requests
   timeout: 5000 // request timeout
 })
