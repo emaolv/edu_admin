11eaf708767c810fab0a2f8481d4a06dd0b4a81e
Jia <233103498@qq.com>
2021-10-28 10:57:37 +0800

    国际化设置 修改语言为zh-CN

--- src/main.js
+++ src/main.js
@@ -4,8 +4,9 @@ import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import locale from 'element-ui/lib/locale/lang/en' // lang i18n
-
+// import locale from 'element-ui/lib/locale/lang/en' // lang i18n
+// 国际化设置 修改语言为zh-CN 使用中文环境
+import locale from 'element-ui/lib/locale/lang/zh-CN' // lang i18n
 import '@/styles/index.scss' // global css
 
 import App from './App'
