682230fdc12f72e7aa14f33821580c5aa3dd82cd
Jia <233103498@qq.com>
2021-10-29 16:13:44 +0800

    增加头像上传

--- src/views/edu/teacher/form.vue
+++ src/views/edu/teacher/form.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="app-container">
+
     <el-form :label-position="labelPosition" label-width="120px" :model="formLabelAlign">
+      <el-form-item label="头像">
+        <!-- action属性定义后台接口地址 :on-success 钩子函数-->
+        <el-upload
+          class="avatar-uploader"
+          action="http://localhost:8120/admin/oss/file/upload?module=avatar"
+          :show-file-list="false"
+          :on-success="handleAvatarSuccess"
+          :before-upload="beforeAvatarUpload"
+        >
+          <img v-if="teacher.avatar" :src="teacher.avatar" class="avatar">
+          <i v-else class="el-icon-plus avatar-uploader-icon" />
+        </el-upload>
+      </el-form-item>
+
       <el-form-item label="讲师名称">
         <el-input v-model="teacher.name" />
       </el-form-item>
@@ -29,7 +44,33 @@
     </el-form>
   </div>
 </template>
+<style>
+  .avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+  .avatar-uploader .el-upload:hover {
+    border-color: #409EFF;
+  }
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+  }
+</style>
 <script>
+
 import teacher from '@/api/teacher'
 
 export default {
@@ -41,7 +82,8 @@ export default {
         level: 1
       },
       // 默认按钮可用
-      saveBtnDisabled: false
+      saveBtnDisabled: false,
+      imageUrl: ''
     }
   },
   created() {
@@ -104,6 +146,28 @@ export default {
             message: '更新失败'
           })
         })
+    },
+    // 文件上传成功
+    handleAvatarSuccess(response) {
+      if (response.success) {
+        this.teacher.avatar = response.data.url
+        this.$forceUpdate()
+      } else {
+        this.$message.error('上传失败')
+      }
+    },
+    // 文件上传前的校验
+    beforeAvatarUpload(file) {
+      const isJPG = file.type === 'image/jpeg'
+      const isLt2M = file.size / 1024 / 1024 < 2
+
+      if (!isJPG) {
+        this.$message.error('上传头像图片只能是 JPG 格式!')
+      }
+      if (!isLt2M) {
+        this.$message.error('上传头像图片大小不能超过 2MB!')
+      }
+      return isJPG && isLt2M
     }
   }
 }
